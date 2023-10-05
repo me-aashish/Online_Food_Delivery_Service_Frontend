3adcffe0a63b3d523deff65d1409b1fbcbd99033
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-10-05 23:58:50 +0530

    created header for our app

--- app.js
+++ app.js
@@ -44,7 +44,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; //to remove warning, use /client
 
+const Title = () => {
+    return (
+       <a href="/"><img src = "https://media.istockphoto.com/id/1194881601/vector/hand-drawn-yummy-face-tongue-smile-delicious-icon-logo.jpg?s=612x612&w=0&k=20&c=Q7Usxg0qyFgZ1fNFG-5arq1FpKCpeoADDfrDC07ITBk=" className="logo" alt="logo"></img></a>
+    );
+};
+
+const Header = () => {
+   
+    return(
+        <div className="header">
+            <Title/>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                    
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <h2>Body</h2>
+    );
+};
+
+const Footer = () => {
+    return (
+        <h2>Footer</h2>
+    );
+};
+
+/**
+ * React.fragment allows to have multiple parent within a component
+ *  - we can use shorthand of React.fragment as <>...</> as JSX expects only one parent.
+ */
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<AppLayout/>);
