5a4a9c01fa3ab117424e4bfab9ac822379b285e6
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-10-06 12:13:51 +0530

    created dynamic data for restaurant list, to work like an API

---
+++ src/constants/RestaurantList.js
@@ -0,0 +1,51 @@
+export const RestaurantList = [
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rkajxxqff1e5msbcmis9",
+        name: "Chandigarh Flavours",
+        cuisines: "Indian, Chinese, Beverages",
+        location: "Sector 46"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rtjayyehxc4e4v7lja6d",
+        name: "Bakingo",
+        cuisines: "Bakery, Desserts",
+        location: "Sector 43"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xwnujftq86flnxlcllgw",
+        name: "Adhikary Foods",
+        cuisines: "Indian, Chinese, Asian",
+        location: "Sector 17"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bhiqtneraycbpeqjworl",
+        name: "Sharma Veg Dhaba",
+        cuisines: "North Indian, Thalis",
+        location: "Mohali Phase 6"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rkajxxqff1e5msbcmis9",
+        name: "Chandigarh Flavours",
+        cuisines: "Indian, Chinese, Beverages",
+        location: "Sector 46"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/da2592e0d25f079cbf9aedf436cabf6f",
+        name: "Shama Biryani",
+        cuisines: "North Indian, Biryanis",
+        location: "Sector 35"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c87ss306gk6akheloq5a",
+        name: "Sundarams",
+        cuisines: "South Indian, Dosas, Idli",
+        location: "Sector 46"
+    },
+    {
+        link: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
+        name: "Pizza",
+        cuisines: "Italian, Pizzas",
+        location: "Sector 22"
+    }
+
+]
